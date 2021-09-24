2ccafb1157cf02a2dc5a0a628a60ad491277f66b
Dhruv <samantdhruv@gmail.com>
2021-09-24 11:15:54 +0530

    logic finally perfected

--- index.js
+++ index.js
@@ -9,7 +9,9 @@ submitBtn.addEventListener("click", submitHandler);
 initialPrice.onkeydown = function(e) {
     if(!((e.keyCode > 95 && e.keyCode < 106)
       || (e.keyCode > 47 && e.keyCode < 58) 
-      || e.keyCode == 8)) {
+      || (e.keyCode == 8)
+      || (e.keyCode == 110)
+      || e.keyCode == 190)) {
         return false;
     }
 }
@@ -25,10 +27,13 @@ stocksQuantity.onkeydown = function(e) {
 currentPrice.onkeydown = function(e) {
     if(!((e.keyCode > 95 && e.keyCode < 106)
       || (e.keyCode > 47 && e.keyCode < 58) 
-      || e.keyCode == 8)) {
+      || (e.keyCode == 8)
+      || (e.keyCode == 110)
+      || e.keyCode == 190)) {
         return false;
     }
 }
+
 
 function submitHandler() {
     var ip = Number(initialPrice.value);
@@ -38,19 +43,23 @@ function submitHandler() {
     calculateProfitAndLoss(ip, qty, curr);
 }
 
+function decimal (x) {
+    return Number.parseFloat(x).toFixed(2);
+}
+
 function calculateProfitAndLoss(initial, quantity, current) {
     if (initial > current) {
-        var loss = (initial - current);
-        var lossPercentage = (loss / initial) * 100;
+        var loss = decimal((initial * quantity) - (current * quantity));
+        var lossPercentage = decimal((loss / (current * quantity)) * 100);
 
-        outputBox.textContent = (`Hey the loss is ${loss} and the percent is ${lossPercentage}%`)
+        outputBox.textContent = (`Hey the invested amount is ${(initial * quantity)} and loss is ${loss} and the percent is ${lossPercentage}%`)
         outputBox.style.color = "red"
 
     } else if (current > initial) {
-        var profit = (current - initial) ;
-        var profitPercentage = (profit / initial) * 100;
+        var profit = decimal((current * quantity) - (initial * quantity));
+        var profitPercentage = decimal((profit / (initial * quantity)) * 100);
 
-        outputBox.textContent = (`Hey the profit is ${profit} and the percent is ${profitPercentage}%`)
+        outputBox.textContent = (`Hey the invested amount is ${(initial * quantity)} profit is ${profit} and the percent is ${profitPercentage}%`)
         outputBox.style.color = "green"
     } else {
 
